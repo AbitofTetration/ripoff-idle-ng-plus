254a09dc80bf030fa46d13d86ff425c9b3cd925a
Glitch (reinhardt-c-ripoff-idle) <none>
2020-04-29 00:21:26 +0000

    🎁🏍 Checkpoint ./js/unfunity.js:2705359/363

--- js/unfunity.js
+++ js/unfunity.js
@@ -7,18 +7,19 @@ function getGalaxyBoost() {
 }
 
 function updateUnfunity(time) {
+  let r = game.tps.pow(-1).pow(0.35)
   let q = game.cookies.log(10).div(25).pow(0.5).div(3600)
   if(game.cookies.gt(1e150)) {
     game.unfunityPoints = game.unfunityPoints.add(q)
   }
-  game.unfunityGalaxies = Decimal.affordGeometricSeries(game.unfunityPoints, new Decimal(5), new Decimal(5), new Decimal(0)).floor()
-  return q.mul(400)
+  game.unfunityGalaxies = Decimal.affordGeometricSeries(game.unfunityPoints, new Decimal(5), new Decimal(4), new Decimal(-1)).floor()
+  return q.mul(400).mul(r)
 }
 
 function getUnfunityBoost() {
   let q = game.unfunityPoints.add(1)
   
-  q = q.max(1).pow(0.65).mul(getGalaxyBoost())
+  q = q.max(1).mul(getGalaxyBoost())
   
   return q
 }
@@ -30,5 +31,5 @@ function showunfunity() {
 
   setElem('unfunGalaxies', game.unfunityGalaxies.toFixed(2))
   setElem('unfunGalaxyBoost', getGalaxyBoost().toFixed(2))
-  setElem('unfunGalaxyThreshold', Decimal.mul(5, Decimal.pow(10, game.unfunityGalaxies)).toFixed(0))
+  setElem('unfunGalaxyThreshold', Decimal.sumGeometricSeries(new Decimal(1), new Decimal(5), new Decimal(4), game.unfunityGalaxies).floor())
 }
