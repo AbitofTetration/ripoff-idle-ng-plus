62dbc52dbe296ce8216c737bfeac3e9db3388f8a
Glitch (reinhardt-c-ripoff-idle) <none>
2020-05-03 02:01:38 +0000

    🍽🌛 Checkpoint ./js/unfunity.js:2705359/511

--- js/unfunity.js
+++ js/unfunity.js
@@ -7,7 +7,7 @@ function getGalaxyBoost() {
 }
 
 function updateUnfunity(time) {
-  let q = game.cookies.log(10).div(15).pow(0.5).div(3600)
+  let q = game.cookies.log(10).div(15).pow(0.5).div(3600).mul(getUnfunityMult())
   if(game.cookies.gt(1e150)) {
     game.unfunityPoints = game.unfunityPoints.add(q)
   }
@@ -26,7 +26,10 @@ function getUnfunityBoost() {
 function getUnfunityMult() {
   let q = new Decimal(1)
   
-  
+  for (let i = 0; i < 16; i++) {
+    let m = game.generators[i][7].amount.div(100000).mul((i*2)+1)
+    q = q.add(m)
+  }
   
   return q
 }
@@ -39,5 +42,5 @@ function showunfunity() {
   setElem('unfunGalaxies', game.unfunityGalaxies.toFixed(2))
   setElem('unfunGalaxyBoost', getGalaxyBoost().toFixed(2))
   setElem('unfunGalaxyThreshold', Decimal.sumGeometricSeries(new Decimal(1), new Decimal(5), new Decimal(4), game.unfunityGalaxies).floor())
-  setElem('unfunityMult', getUnfunityMult())
+  setElem('unfunityMult', getUnfunityMult().toFixed(2))
 }
