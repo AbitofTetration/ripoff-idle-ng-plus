ce021a6e3c75b095c85447b1dd1cc7e126c48568
Glitch (reinhardt-c-ripoff-idle) <none>
2020-05-09 23:57:48 +0000

    ☎️🌲 Checkpoint ./js/achievement.js:2705359/141 ./js/popup.js:2705359/3884

--- js/popup.js
+++ js/popup.js
@@ -39,3 +39,40 @@ class AchievementPopup {
 		}, 5000);
 	}
 }
+class TipPopup {
+	constructor(id) {
+		this.name = id
+	}
+	
+	popup() {
+		let exists = true;
+		let d = document.createElement('div');
+		d.className = 'popup';
+		
+		let txt = document.createElement('p');
+		txt.innerHTML = this.name;
+		txt.style.marginLeft = '5px';
+		txt.style.fontSize = '15px';
+		d.appendChild(txt);
+		d.addEventListener('click', function() {
+			exists = false;
+			document.getElementById('popups').removeChild(d);
+			delete this;
+		});
+		document.getElementById('popups').appendChild(d);
+		
+		setTimeout(function() {
+			if (exists) {
+				exists = false;
+				document.getElementById('popups').removeChild(d);
+				delete this;
+			}
+		}, 5000);
+	}
+}
+
+function tip() {
+  let tip = ['Buying more 8th tier buildings boosts Unfunity Point gain.', 'When in doubt, grind unfunity points.'][Math.floor(Math.random()*2)]
+  new TipPopup(tip).popup();
+}
+setInterval(tip(), 5000);
