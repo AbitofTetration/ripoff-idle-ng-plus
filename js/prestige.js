06dcb4a407f8e2126eda806ad181cbed1cabea0a
Glitch (reinhardt-c-ripoff-idle) <none>
2020-05-21 23:58:19 +0000

    💋💸 Checkpoint ./js/prestige.js:2705359/49

--- js/prestige.js
+++ js/prestige.js
@@ -80,7 +80,7 @@ function getPrestigeBoosts(number) {
       return Decimal.pow(2, game.prestigeUpgrades[3])
       break;
     case 5:
-      return Decimal.pow(game.prestiges.pow(0.02), game.prestigeUpgrades[4]).max(1)
+      return Decimal.pow(game.prestiges.pow(0.02).add(1), game.prestigeUpgrades[4]).max(1)
       break;
   }
 }
