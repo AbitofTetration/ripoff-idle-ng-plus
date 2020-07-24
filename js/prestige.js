3b12673be81cb02889ca94c6d19deff7e6926351
AbitofTetration <58894120+AbitofTetration@users.noreply.github.com>
2020-07-24 14:02:07 -0500

    Update prestige.js

--- js/prestige.js
+++ js/prestige.js
@@ -53,7 +53,7 @@ function getPrestigePointPower() {
 }
 
 function getPrestigeCost(number) {
-  let q = new Decimal([1,3,5,1,5,10,8,10][number-1]).mul(Decimal.pow([5,5,5,10,5,Infinity,8,Infinity][number-1], game.prestigeUpgrades[number-1]))
+  let q = new Decimal([1,2,3,1,3,5,5,15][number-1]).mul(Decimal.pow([5,5,5,10,5,Infinity,8,Infinity][number-1], game.prestigeUpgrades[number-1]))
   
   return q
 }
