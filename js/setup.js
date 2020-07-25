0276217b8106063138fc3ff17c89fcc7924ce065
AbitofTetration <58894120+AbitofTetration@users.noreply.github.com>
2020-07-24 22:08:41 -0500

    Begin revamp of achievements

--- js/setup.js
+++ js/setup.js
@@ -1605,252 +1605,9 @@ class Game {
 				visReq: () => ggte(15, 7, 1),
 				purpose: 'Multiplies fractal engine efficiency by 8.'
 			},
-      // Unfunity upgrades
-			{
-				name: 'Unfun Cookies',
-				cost: new Decimal('1e250'),
-				icon: 'assets/img/cookie.png',
-				onBuy: () => mag(15, 8),
-				visReq: () => ggte(15, 7, 1),
-				purpose: 'Multiplies unfunity power gain by 2.'
-			}
 		]
 		
-		this.achievementData = [
-			// Cookie Achievements
-			{
-				name: 'I Have a Cookie',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(1),
-				unlockTxt: 'Baked 1 cookie'
-			},
-			{
-				name: 'Double the Calories',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(2),
-				unlockTxt: 'Baked 2 cookies'
-			},
-			{
-				name: 'I Need More Chocolate Chips',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(10),
-				unlockTxt: 'Baked 10 cookies'
-			},
-			{
-				name: 'Sugar Craving',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(25),
-				unlockTxt: 'Bake 25 cookies'
-			},
-			{
-				name: 'Addiction',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(50),
-				unlockTxt: 'Bake 50 cookies'
-			},
-			{
-				name: 'You Can\'t Stop Now!',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(100),
-				unlockTxt: 'Bake 100 cookies'
-			},
-			{
-				name: 'Well Yes, But Actually Cookies',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(250),
-				unlockTxt: 'Bake 250 cookies'
-			},
-			{
-				name: '<strike>STONKS</strike> COOKIES',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(500),
-				unlockTxt: 'Bake 500 cookies'
-			},
-			{
-				name: 'New SI Unit: Kilocookie',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(1000),
-				unlockTxt: 'Bake 1K cookies'
-			},
-			{
-				name: 'Much Cookie',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(5000),
-				unlockTxt: 'Bake 5K cookies'
-			},
-			{
-				name: 'Only 102% Sugar!',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(10000),
-				unlockTxt: 'Bake 10K cookies'
-			},
-			{
-				name: 'Just One More',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(100000),
-				unlockTxt: 'Bake 100K cookies'
-			},
-			{
-				name: 'New SI Unit: Megacookie',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => tcgte(1000000),
-				unlockTxt: 'Bake 1M cookies'
-			},
-			// 1D Cursors
-			{
-				name: 'Click',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 0, 1),
-				unlockTxt: 'Have a 1D cursor'
-			},
-			{
-				name: 'Double Click',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 0, 2),
-				unlockTxt: 'Have 2 1D cursors'
-			},
-			{
-				name: 'Look Ma, Two Hands!',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 0, 10),
-				unlockTxt: 'Have 10 1D cursors'
-			},
-			{
-				name: '5 Hands is Better Than 1',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 0, 25),
-				unlockTxt: 'Have 25 1D cursors'
-			},
-			{
-				name: 'Fifty Fingers',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 0, 50),
-				unlockTxt: 'Have 50 1D cursors'
-			},
-			{
-				name: 'Hundred Fingers',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 0, 100),
-				unlockTxt: 'Have 100 1D cursors'
-			},
-			// All Cursors
-			{
-				name: 'Slap',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 1, 1),
-				unlockTxt: 'Have 1 2D cursor'
-			},
-			{
-				name: 'With (not really) 3D Graphics',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 2, 1),
-				unlockTxt: 'Have 1 3D cursor'
-			},
-			{
-				name: 'Stereographic Projection',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 3, 1),
-				unlockTxt: 'Have 1 4D cursor'
-			},
-			{
-				name: 'Orthographic Projection',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 4, 1),
-				unlockTxt: 'Have 1 5D cursor'
-			},
-			{
-				name: 'Projection Matrices',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 5, 1),
-				unlockTxt: 'Have 1 6D cursor'
-			},
-			{
-				name: 'Sphere Inside the Outside of a Sphere',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 6, 1),
-				unlockTxt: 'Have 1 7D cursor'
-			},
-			{
-				name: 'Click Lattice',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(0, 7, 1),
-				unlockTxt: 'Have 1 8D cursor'
-			},
-			// 1D Grandmas
-			{
-				name: 'Nice Grandma',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 0, 1),
-				unlockTxt: 'Have a 1D grandma'
-			},
-			{
-				name: 'Nice Grandmas',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 0, 10),
-				unlockTxt: 'Have 10 1D grandmas'
-			},
-			{
-				name: 'Lots of Nice Grandmas',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 0, 25),
-				unlockTxt: 'Have 25 1D grandma'
-			},
-			{
-				name: 'Too Many Nice Grandmas',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 0, 50),
-				unlockTxt: 'Have 50 1D grandmas'
-			},
-			{
-				name: 'Death by Warm Hugs',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 0, 100),
-				unlockTxt: 'Have 100 1D grandmas'
-			},
-			// All Grandmas
-			{
-				name: 'Square Glasses',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 1, 1),
-				unlockTxt: 'Have a 2D grandma'
-			},
-			{
-				name: 'Knitted Sweaters',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 2, 1),
-				unlockTxt: 'Have a 3D grandma'
-			},
-			{
-				name: 'Propreantepenultimate Grandma',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 3, 1),
-				unlockTxt: 'Have a 4D grandma'
-			},
-			{
-				name: 'Preantepenultimate Grandma',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 4, 1),
-				unlockTxt: 'Have a 5D grandma'
-			},
-			{
-				name: 'Antepenultimate Grandma',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 5, 1),
-				unlockTxt: 'Have a 6D grandma'
-			},
-			{
-				name: 'Penultimate Grandma',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 6, 1),
-				unlockTxt: 'Have a 7D grandma'
-			},
-			{
-				name: 'Ultimate Grandma',
-				icon: 'assets/img/cookie.png',
-				unlockReq: () => ggte(1, 4, 1),
-				unlockTxt: 'Have an 8D grandma<br>"This isn\'t even my final form!"'
-			}
-		]
+
 		
 		// End Data Objects
 		
@@ -1864,10 +1621,6 @@ class Game {
 		this.achievements = [];
 		this.unlockedAchievements = 0;
 		
-		for (let i = 0; i < this.achievementData.length; i++) {
-			this.achievements[i] = new Achievement(i, this.achievementData[i].name, this.achievementData[i].icon, this.achievementData[i].unlockReq, false);
-		}
-		
 		this.buyAmount = new Decimal(1);
 		document.getElementById('bulk').value = this.buyAmount;
 		this.buymax = false;
@@ -1880,7 +1633,7 @@ class Game {
 		this.autosaveintv = 10;
 		document.getElementById('asintv').value = this.autosaveintv;
 
-		this.vers = '0.2.1';
+		this.vers = '0.2.2';
 		
 		if (data != undefined) {
 			try {
@@ -1927,13 +1680,12 @@ class Game {
 					this.upgrades[i] = new Upgrade(this.upgradeData[i].name, this.upgradeData[i].cost, data.upgrades[i].bought, this.upgradeData[i].icon, this.upgradeData[i].onBuy, this.upgradeData[i].visReq);
 				}
 				
-				for (let i = 0; i < data.achievements.length; i++) {
-					if (data.achievements[i].unlocked) {
-						this.unlockedAchievements++;
-					}
-					this.achievements[i] = new Achievement(i, this.achievementData[i].name, this.achievementData[i].icon, this.achievementData[i].unlockReq, data.achievements[i].unlocked);
+				if (vers != '0.2.1') {
+				   if (data.achievements[i] == true) {
+					this.achievements[i] == true
+				   }
 				}
-				
+					
 				this.buyAmount = new Decimal(data.buyAmount);
 				document.getElementById('bulk').value = this.buyAmount;
 				this.buymax = data.buymax;
