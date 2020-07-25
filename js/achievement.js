6ff7e7b5c3e2927648aaf79c9e5b3e7b67f1a5d0
AbitofTetration <58894120+AbitofTetration@users.noreply.github.com>
2020-07-24 22:14:40 -0500

    doot zoot doot

--- js/achievement.js
+++ js/achievement.js
@@ -16,3 +16,15 @@ class Achievement {
 		return this.unlockReq();
 	}
 }
+
+let Achievements = [
+	// row 1, early game
+	["The beginning", "Earn 1 cookie."],
+	["Click click", "Get 1 1d cursor."],
+	["Rolling pins", "Get 1 1d grandma."],
+	["Cookie wheat", "Get 1 1d farm."],
+	["The real grind", "Earn 1e20 cookies. Reward: +1% cookie gain."],
+	["Click click click", "Click 500 times. Reward: You gain +1% of CpS per click."],
+	["That was unfun", "Unlock unfunity."],
+	// row 2, near prestige
+	["The realer grind", "Earn 1e250 cookies. Reward: +1% cookie gain."],
