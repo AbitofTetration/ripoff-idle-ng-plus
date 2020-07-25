7a4f1e14aaba1755d6d2554d8f9db13148a38f44
AbitofTetration <58894120+AbitofTetration@users.noreply.github.com>
2020-07-24 22:19:56 -0500

    diamond pickaxe achievement

--- js/achievement.js
+++ js/achievement.js
@@ -23,8 +23,27 @@ let Achievements = [
 	["Click click", "Get 1 1d cursor."],
 	["Rolling pins", "Get 1 1d grandma."],
 	["Cookie wheat", "Get 1 1d farm."],
+	["Diamond pickaxe", "Get 1 1d mine."],
 	["The real grind", "Earn 1e20 cookies. Reward: +1% cookie gain."],
 	["Click click click", "Click 500 times. Reward: You gain +1% of CpS per click."],
 	["That was unfun", "Unlock unfunity."],
 	// row 2, near prestige
 	["The realer grind", "Earn 1e250 cookies. Reward: +1% cookie gain."],
+]
+function giveAchievement(id) {
+	if(game.achievements.includes(id)) return;
+	game.achievements.push(id);
+	updateAchievements();
+}
+
+function updateAchievements() {
+	updateAchievementDescriptions();
+	
+	for(var i = 0; i < achievements; i++) {
+		var a = ge("achievement" + i)
+		a.className = game.achievements.includes(i) ? "achievementunlocked" : "achievementlocked"
+		a.innerHTML = "<br>" + achievementDescriptions[i * 2] + "<br>" + (devMode ? i : "") 
+		a.setAttribute("tooltip", achievementDescriptions[i * 2 + 1])
+		a.style.zIndex = 1e6-i;
+	}
+}
